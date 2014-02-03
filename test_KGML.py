f237e9104f1f73f2b773c176fecd17e188a01b17
widdowquinn <leightonpritchard@mac.com>
2014-02-03 21:24:34 +0000

    PEP8 corrections to test_KGML.py.

--- test_KGML.py
+++ test_KGML.py
@@ -29,7 +29,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-""" Tests for general functionality of the KGML parser, pathway and 
+""" Tests for general functionality of the KGML parser, pathway and
     visualisation modules
 """
 
@@ -47,10 +47,11 @@ try:
     from reportlab.lib.pagesizes import A4
 except ImportError:
     raise MissingPythonDependencyError(
-            "Install reportlab if you want to use Bio.Graphics.")
+        "Install reportlab if you want to use Bio.Graphics.")
 
 # Do we have internet (code from Biopython's requires_internet.py)
 from Bio import MissingExternalDependencyError
+
 
 def check_internet():
     try:
@@ -76,6 +77,7 @@ from KGML_parser import read
 from KGML_vis import KGMLCanvas
 from KGML_scrape import retrieve_kgml_to_file, retrieve_KEGG_pathway
 
+
 class PathwayData(object):
     """ Convenience structure for testing pathway data
     """
@@ -86,6 +88,7 @@ class PathwayData(object):
         self.element_counts = element_counts
         self.pathway_image = pathway_image
         self.show_pathway_image = show_pathway_image
+
 
 class KGMLPathwayTest(unittest.TestCase):
     """ Import the ko01100 metabolic map from a local .xml KGML file, and from
@@ -108,24 +111,26 @@ class KGMLPathwayTest(unittest.TestCase):
                                  (81, 72, 8, 1),
                                  os.path.join("KEGG", "map03070.png"),
                                  True)]
-        # A list of KO IDs that we're going to use to modify pathway 
-        # appearance. These are KO IDs for reactions that take part in ko00020, 
+        # A list of KO IDs that we're going to use to modify pathway
+        # appearance. These are KO IDs for reactions that take part in ko00020,
         # the TCA cycle
         self.ko_ids = \
-            set(['ko:K00239','ko:K00240','ko:K00241','ko:K00242','ko:K00244',
-                 'ko:K00245','ko:K00246','ko:K00247','ko:K00174','ko:K00175',
-                 'ko:K00177','ko:K00176','ko:K00382','ko:K00164','ko:K00164',
-                 'ko:K00658','ko:K01902','ko:K01903','ko:K01899','ko:K01900',
-                 'ko:K01899','ko:K01900','ko:K00031','ko:K00030','ko:K00031',
-                 'ko:K01648','ko:K00234','ko:K00235','ko:K00236','ko:K00237',
-                 'ko:K01676','ko:K01677','ko:K01678','ko:K01679','ko:K01681',
-                 'ko:K01682','ko:K01681','ko:K01682','ko:K01647','ko:K00025',
-                 'ko:K00026','ko:K00024','ko:K01958','ko:K01959','ko:K01960',
-                 'ko:K00163','ko:K00161','ko:K00162','ko:K00163','ko:K00161',
-                 'ko:K00162','ko:K00382','ko:K00627','ko:K00169','ko:K00170',
-                 'ko:K00172','ko:K00171','ko:K01643','ko:K01644','ko:K01646',
-                 'ko:K01610','ko:K01596'])
-
+            set(['ko:K00239', 'ko:K00240', 'ko:K00241', 'ko:K00242',
+                 'ko:K00244', 'ko:K00245', 'ko:K00246', 'ko:K00247',
+                 'ko:K00174', 'ko:K00175', 'ko:K00177', 'ko:K00176',
+                 'ko:K00382', 'ko:K00164', 'ko:K00164', 'ko:K00658',
+                 'ko:K01902', 'ko:K01903', 'ko:K01899', 'ko:K01900',
+                 'ko:K01899', 'ko:K01900', 'ko:K00031', 'ko:K00030',
+                 'ko:K00031', 'ko:K01648', 'ko:K00234', 'ko:K00235',
+                 'ko:K00236', 'ko:K00237', 'ko:K01676', 'ko:K01677',
+                 'ko:K01678', 'ko:K01679', 'ko:K01681', 'ko:K01682',
+                 'ko:K01681', 'ko:K01682', 'ko:K01647', 'ko:K00025',
+                 'ko:K00026', 'ko:K00024', 'ko:K01958', 'ko:K01959',
+                 'ko:K01960', 'ko:K00163', 'ko:K00161', 'ko:K00162',
+                 'ko:K00163', 'ko:K00161', 'ko:K00162', 'ko:K00382',
+                 'ko:K00627', 'ko:K00169', 'ko:K00170', 'ko:K00172',
+                 'ko:K00171', 'ko:K01643', 'ko:K01644', 'ko:K01646',
+                 'ko:K01610', 'ko:K01596'])
 
     def test_read_and_write_KGML_files(self):
         """ Read KGML from, and write KGML to, local files.
@@ -136,7 +141,7 @@ class KGMLPathwayTest(unittest.TestCase):
             with open(p.infilename, 'rU') as f:
                 pathway = read(f)
                 # Do we have the correct number of elements of each type
-                self.assertEqual((len(pathway.entries), 
+                self.assertEqual((len(pathway.entries),
                                   len(pathway.orthologs),
                                   len(pathway.compounds),
                                   len(pathway.maps)),
@@ -148,17 +153,16 @@ class KGMLPathwayTest(unittest.TestCase):
             with open(p.outfilename, 'rU') as f:
                 pathway = read(f)
                 # Do we have the correct number of elements of each type
-                self.assertEqual((len(pathway.entries), 
+                self.assertEqual((len(pathway.entries),
                                   len(pathway.orthologs),
                                   len(pathway.compounds),
                                   len(pathway.maps)),
                                  p.element_counts)
 
-
     def test_render_KGML_basic(self):
         """ Basic rendering of KGML: write to PDF without modification.
         """
-        # We test rendering of both the original KEGG KGML and the 
+        # We test rendering of both the original KEGG KGML and the
         # roundtrip KGML written by this module, using only local files.
         for p in self.data:
             for filename, ext in [(p.infilename, '_original.pdf'),
@@ -169,7 +173,6 @@ class KGMLPathwayTest(unittest.TestCase):
                     kgml_map = KGMLCanvas(pathway)
                     kgml_map.import_imagemap = p.show_pathway_image
                     kgml_map.draw(os.path.splitext(filename)[0] + ext)
-        
 
     def test_render_KGML_modify(self):
         """ Rendering of KGML to PDF, with modification.
@@ -181,8 +184,8 @@ class KGMLPathwayTest(unittest.TestCase):
                               (p.outfilename, '_mod_roundtrip.pdf')]:
             with open(filename) as f:
                 pathway = read(f)
-                mod_rs = [e for e in pathway.orthologs if \
-                        len(set(e.name.split()).intersection(self.ko_ids))]
+                mod_rs = [e for e in pathway.orthologs if
+                          len(set(e.name.split()).intersection(self.ko_ids))]
                 for r in mod_rs:
                     for g in r.graphics:
                         g.width = 10
@@ -197,7 +200,7 @@ class KGMLPathwayTest(unittest.TestCase):
                 pathway = read(f)
                 orthologs = [e for e in pathway.orthologs]
                 # Use Biopython's ColorSpiral to generate colours
-                cs = ColorSpiral(a=2, b=0.2, v_init=0.85, v_final=0.5, 
+                cs = ColorSpiral(a=2, b=0.2, v_init=0.85, v_final=0.5,
                                  jitter=0.03)
                 colors = cs.get_colors(len(orthologs))
                 for o, c in zip(orthologs, colors):
@@ -218,9 +221,7 @@ class KGMLPathwayTest(unittest.TestCase):
         kgml_map.draw(os.path.join('KEGG', 'ko01110.pdf'))
         # Download the KEGG ko01120 pathway and write to file as KGML
         retrieve_kgml_to_file("ko01120", os.path.join("KEGG", "ko01120.xml"))
-        
-
 
 if __name__ == '__main__':
-    runner = unittest.TextTestRunner(verbosity = 2)
-    unittest.main(testRunner = runner)
+    runner = unittest.TextTestRunner(verbosity=2)
+    unittest.main(testRunner=runner)
